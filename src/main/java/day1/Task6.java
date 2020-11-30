6de5939291caa4df4b1dffed81d1bcc871514498
AgaevAlex <agaevalex97@gmail.com>
2020-11-30 22:29:07 +0300

    Add Task2 solution

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 1;
+        for (int count = 0; count < 10; count++) {
+            System.out.println(count + " * " + k + " = " + count * k);
+        }
 
     }
 }
