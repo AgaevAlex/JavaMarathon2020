d919df1aa8c8aa49ea264d58c87d5a6494d9f56a
AgaevAlex <agaevalex97@gmail.com>
2020-12-02 01:30:04 +0300

    Add Task2 solution

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int result = scanner.nextInt();
+        if (result >= 1 && result <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (result >= 5 && result <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (result >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
