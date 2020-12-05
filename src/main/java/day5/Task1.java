ccbf135cc00c3818fdac84d57ef853f6e03e161f
AgaevAlex <agaevalex97@gmail.com>
2020-12-05 15:52:04 +0300

    Add Task5 solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,39 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("Toyota");
+        car.setDate(2004);
+        car.setColor("Red");
+        System.out.println("Модель машины: " + car.getModel() + ". Цвет машины: " + car.getColor() + ". Год выпуска: " + car.getDate());
+    }
+}
 
+class Car {
+    private String color, model;
+    private int date;
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,30 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motobike motobike = new Motobike("Blue", "Yamaha", 2020);
+        System.out.println("Модель байка: " + motobike.getModel() + ". Цвет байка: " + motobike.getColor() + ". Год выпуска: " + motobike.getData());
+    }
+}
 
+class Motobike {
+    private String color, model;
+    int data;
+
+    public Motobike(String color, String model, int data) {
+        this.color = color;
+        this.model = model;
+        this.data = data;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getData() {
+        return data;
     }
 }
