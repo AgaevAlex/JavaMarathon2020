2f2eb3cd7fed3f83ebeb197ca8ae7fc4fdd0f0e0
AgaevAlex <agaevalex97@gmail.com>
2020-12-10 00:10:12 +0300

    Add Task4 solution

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -6,23 +6,23 @@ public class Task4 {
     public static void main(String[] args) {
         final int CONST = 50;
         int[] results = new int[100000];
-        int sum = 0, sum2 = 0, index = 0, count = 0;
+        int sum = 0, sum2 = 0, index = 0;
         Random random = new Random();
         for (int i = 0; i < results.length; i++) {
             results[i] = random.nextInt(10);
         }
-        while (count < (results.length % CONST == 0 ? results.length - CONST : results.length - CONST + 1)) {
-            for (int y = 0; y < CONST; y++) {
-                sum += results[count++];
+
+        for (int count = 0; count < results.length - CONST + 1; count++) {
+            for (int count2 = 0; count2 < CONST; count2++) {
+                sum += results[count + count2];
             }
             if (sum > sum2) {
-                index = count;
                 sum2 = sum;
+                index = count;
             }
-            count -= CONST - 1;
             sum = 0;
         }
         System.out.println(sum2);
-        System.out.println(index - CONST);
+        System.out.println(index);
     }
 }
